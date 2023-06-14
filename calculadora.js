af54763ac688969a6a0adba2d7b5286d93d7bf81
campos845 <136643376+campos845@users.noreply.github.com>
2023-06-14 16:46:58 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,156 @@
+function soma() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+}
+
+  function soma() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+  }
+
+  function soma() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 + num2;
+    document.getElementById("resultado").innerHTML = "Resultado:" + resultado;
+  }
+
+  function soma() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 + num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function subtrai() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 - num2;
+    document.getElementById("resultado").innerHTML = "Resultado:" + resultado;
+  }
+
+  function soma() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 + num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function subtrai() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 - num2;
+    document.getElementById("resultado").innerHTML = "Resultado:" + resultado;
+  }
+
+  function soma() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 + num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function subtrai() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 - num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function multiplica() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 * num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function divide() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 / num2;
+    document.getElementById("resultado").innerHTML = "Resultado:" + resultado;
+}
+
+  function soma() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 + num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function subtrai() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 - num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function multiplica() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 * num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function divide() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 / num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado; 
+}
+
+  function soma() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 + num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function subtrai() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 - num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function multiplica() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 * num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function divide() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 / num2;
+    document.getElementById("resultado").innerHTML = "Resultado" + resultado; 
+}
+
+  function soma() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 + num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function subtrai() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 - num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function multiplica() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 * num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+  }
+
+  function divide() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 / num2;
+    document.getElementById("resultado").innerHTML = "resultado: " + resultado;
+}
